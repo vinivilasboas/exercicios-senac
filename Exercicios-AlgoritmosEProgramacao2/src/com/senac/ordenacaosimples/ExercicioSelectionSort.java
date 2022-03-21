199d1e025c29b450a9858e518bc0995c2bf27f55
vinivilasboas <vinivilasboas@gmail.com>
2022-03-21 11:06:48 -0300

    adição de exercício de selection sort

--- Exercicios-AlgoritmosEProgramacao2/src/com/senac/ordenacaosimples/ExercicioSelectionSort.java
+++ Exercicios-AlgoritmosEProgramacao2/src/com/senac/ordenacaosimples/ExercicioSelectionSort.java
@@ -7,34 +7,38 @@ import java.util.Arrays;
  * @author vinivilasboas
  */
 
-public class ExercicioInsertionSort {
+public class ExercicioSelectionSort {
 
     public static void main(String[] args) {
 
         int[] vetorA = {36, 15, 75, 2, 16, 48, 42, 51};
 
-        insertionSort(vetorA);
+        selectionSort(vetorA);
 
         int[] vetorB = {13, 45, 38, 20, 23, 7, 6, 5, 10, 11};
 
-        insertionSort(vetorB);
+        selectionSort(vetorB);
 
         int[] vetorC = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        insertionSort(vetorC);
+        selectionSort(vetorC);
 
     }
 
-    public static void insertionSort (int[] vetor) {
+    public static void selectionSort(int[] vetor) {
 
         System.out.println("vetor inicial: " + Arrays.toString(vetor));
 
-        for (int i = 1; i < vetor.length; i++) {
-            int n = vetor[i];
-            for (int j = i - 1; j >= 0 && vetor[j] > n; j--) {
-                vetor[j + 1] = vetor[j];
-                vetor[j] = n;
+        for (int i = 0; i < vetor.length; i++) {
+            int menor = i;
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[j] < vetor[menor]) {
+                    menor = j;
+                }
             }
+            int n = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = n;
             System.out.println("laço " + i + ": " + Arrays.toString(vetor));
         }
 
